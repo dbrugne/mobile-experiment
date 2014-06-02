1e383671144afaf81ffcf0707469ed0c5be98c18
damien.brugne <damien.brugne@gmail.com>
2014-06-02 18:57:38 +0200

    Implement native notifications

--- www/js/app.js
+++ www/js/app.js
@@ -1,30 +1,42 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function () {
 
-    /* ---------------------------------- Local Variables ---------------------------------- */
-    var adapter = new LocalStorageAdapter();
-    adapter.initialize().done(function () {
-        console.log("Data adapter initialized");
-    });
+  /* ---------------------------------- Local Variables ---------------------------------- */
+  var adapter = new LocalStorageAdapter();
+  adapter.initialize().done(function () {
+      console.log("Data adapter initialized");
+  });
 
-    /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
-        alert("Some help here...")
-    });
+  /* --------------------------------- Event Registration -------------------------------- */
+  $('.search-key').on('keyup', findByName);
+  $('.help-btn').on('click', function() {
+      alert("Some help here...")
+  });
 
+  /* ---------------------------------- Local Functions ---------------------------------- */
+  function findByName() {
+      adapter.findByName($('.search-key').val()).done(function (employees) {
+          var l = employees.length;
+          var e;
+          $('.employee-list').empty();
+          for (var i = 0; i < l; i++) {
+              e = employees[i];
+              $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+          }
+      });
+  }
 
-    /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        adapter.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+  document.addEventListener('deviceready', function () {
+    if (navigator.notification) { // Override default HTML alert with native dialog
+      window.alert = function (message) {
+        navigator.notification.alert(
+          message,    // message
+          null,       // callback
+          "Workshop", // title
+          'OK'        // buttonName
+        );
+      };
     }
+  }, false);
 
 }());

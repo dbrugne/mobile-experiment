337cd0e1af7f106430f943a90d4c0c8b4c634c2c
damien.brugne <damien.brugne@gmail.com>
2014-06-02 21:44:21 +0200

    Implement native scrool

--- www/js/app.js
+++ www/js/app.js
@@ -6,8 +6,7 @@
   var employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
   var adapter = new LocalStorageAdapter();
   adapter.initialize().done(function () {
-    console.log("Data adapter initialized");
-    renderHomeView();
+    $('body').html(new HomeView(adapter, homeTpl, employeeLiTpl).render().el);
   });
 
   /* --------------------------------- Event Registration -------------------------------- */
@@ -26,15 +25,6 @@
   }, false);
 
   /* ---------------------------------- Local Functions ---------------------------------- */
-  function findByName() {
-      adapter.findByName($('.search-key').val()).done(function (employees) {
-        $('.employee-list').html(employeeLiTpl(employees));
-      });
-  }
 
-  function renderHomeView() {
-    $('body').html(homeTpl());
-    $('.search-key').on('keyup', findByName);
-  }
 
 }());
